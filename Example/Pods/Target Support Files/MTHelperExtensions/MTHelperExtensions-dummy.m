92340754d0464ad5a3148c3a2531a77066744a7c
meerantariq <meerantariq5432@gmail.com>
2019-10-08 16:19:41 +0500

    Added boilerplate for MTHelperExtensions pod

---
+++ Example/Pods/Target Support Files/MTHelperExtensions/MTHelperExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MTHelperExtensions : NSObject
+@end
+@implementation PodsDummy_MTHelperExtensions
+@end
---
+++ Example/Pods/Target Support Files/Pods-MTHelperExtensions_Tests/Pods-MTHelperExtensions_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MTHelperExtensions_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MTHelperExtensions_Tests
+@end
